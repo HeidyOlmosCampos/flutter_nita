b1722dea2fbd9570666412a2bccc3f5c3df2115f
Evankhell <80553626+HeidyOlmosCampos@users.noreply.github.com>
2023-02-22 06:51:18 -0400

    send email SOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 
 // import 'package:app_launcher/app_launcher.dart';
@@ -10,12 +11,21 @@ import 'package:flutter_background_service/flutter_background_service.dart'
         ServiceInstance;
         
 import 'package:flutter/material.dart';
+
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart'; 
+
 // import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'background_service.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:dialog_flowtter/dialog_flowtter.dart';
+import 'package:http/http.dart' as http;
+
+
+
+// import 'package:flutter_sms/flutter_sms.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,16 +50,18 @@ class _MyAppState extends State<MyApp> {
   final flutterTts = FlutterTts();
   late DialogFlowtter dialogFlowtter;
   SpeechToText _speechToText = SpeechToText();
-
   final TextEditingController _controller = TextEditingController();
+  final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
 
   String text = "Stop Service";
+
 
 
   @override
   void initState() {
     DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
-   _initSpeech();
+    _initSpeech();
+    _initLocation();
     super.initState();
   }
 
@@ -60,6 +72,8 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 171, 157, 209),
         body: Center(
+
+
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -114,6 +128,14 @@ class _MyAppState extends State<MyApp> {
                   height: 100,
                 ),
               ),
+
+              // FloatingActionButton(
+              //   onPressed: _sendEmail,
+              //           // _sendSMS,
+              //           // _location,
+              //           //_speechToText.isNotListening ? _startListening : _stopListening,
+              //   tooltip: 'Listen',
+              // ),
               
 
             ],
@@ -124,12 +146,47 @@ class _MyAppState extends State<MyApp> {
   }
 
 
+  // void _sendEmail(){
+  //   var
+  //     Service_id='service_emp11gu',
+  //     Template_id='template_t12oyse',
+  //     User_id='Tz-1eK_bjtePLr2Tu';
+  //   var s = http.post(Uri.parse('https://api.emailjs.com/api/v1.0/email/send'),
+  //   headers: {
+  //       'origin':'http:localhost',
+  //       'Content-Type':'application/json'
+  //   },
+  //   body: jsonEncode({
+  //     'service_id': Service_id,
+  //     'user_id': User_id,
+  //     'template_id': Template_id,
+  //     'template_params':{
+  //       'name': 'Juan',
+  //       'name_contact': 'Heidy',
+  //       'message': 'SOS. Mi ubicacion actual:',
+  //       'sender_email': 'holmoscampos@gmail.com'
+  //     }
+  //   })
+  //   );
+  // }
+
+
+  
+
+
 
   /// Esto tiene que suceder solo una vez por aplicación
   void _initSpeech() async {
     await _speechToText.initialize();
-    // _speechEnabled = await _speechToText.initialize();
-    // setState(() {});
+  }
+
+
+  void _initLocation() async {
+    LocationPermission permission;
+    permission = await _geolocatorPlatform.checkPermission();
+    if(permission == LocationPermission.denied) {
+    permission = await _geolocatorPlatform.requestPermission();
+    }
   }
 
 
