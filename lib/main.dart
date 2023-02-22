b4144c7e5a7bb79d529d234181a3b01d243e2ea1
DAVV422 <diegoalberto42216@gmail.com>
2023-02-22 07:14:20 -0400

    arreglo

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,28 @@
 import 'dart:async';
-import 'dart:convert';
 
-// import 'package:app_launcher/app_launcher.dart';
+
+import 'package:app_launcher/app_launcher.dart';
 import 'package:flutter_background_service/flutter_background_service.dart'
     show
         AndroidConfiguration,
         FlutterBackgroundService,
         IosConfiguration,
         ServiceInstance;
-
+        
 import 'package:flutter/material.dart';
-
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart'; 
-
-// import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'background_service.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:dialog_flowtter/dialog_flowtter.dart';
-import 'package:http/http.dart' as http;
-
-
-
-// import 'package:flutter_sms/flutter_sms.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeService();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatefulWidget {
   const MyApp({
@@ -41,24 +33,26 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+
+
 class _MyAppState extends State<MyApp> {
 
 
   final flutterTts = FlutterTts();
   late DialogFlowtter dialogFlowtter;
   SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+
   final TextEditingController _controller = TextEditingController();
-  final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
+  
 
   String text = "Stop Service";
-
 
 
   @override
   void initState() {
     DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
     _initSpeech();
-    _initLocation();
     super.initState();
   }
 
@@ -67,10 +61,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 171, 157, 209),
+        backgroundColor: Color.fromARGB(255, 168, 146, 228),
         body: Center(
-
-
+          // child: Chat(),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -98,22 +91,26 @@ class _MyAppState extends State<MyApp> {
               //   }
               // ),
               Expanded(
-                child: Container(
+                child: Container( 
                   width: double.infinity,
                   height: 100,
-                  alignment: Alignment.center,
-                  child: Text("NITA",
-                      style: TextStyle(
-                        fontSize: 100,
-                        foreground: Paint()
-                          ..style = PaintingStyle.stroke
-                          ..strokeWidth = 11
-                          ..color = const Color.fromARGB(255, 16, 26, 36),
-                      )),
+                  alignment: Alignment.center, 
+                  child: Text(
+                    "NITA",
+                    style: TextStyle(
+                      fontSize: 100,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 11
+                        ..color = const Color.fromARGB(255, 16, 26, 36),
+                    )
+                  ),
                 ),
               ),
               const Expanded(
-                child: Image(image: AssetImage('assets/bruce.png')),
+                child: Image(
+                  image: AssetImage('assets/bruce.png')
+                ),
               ),
               Expanded(
                 child: SvgPicture.asset(
@@ -121,15 +118,34 @@ class _MyAppState extends State<MyApp> {
                   height: 100,
                 ),
               ),
-
-              // FloatingActionButton(
-              //   onPressed: _sendEmail,
-              //           // _sendSMS,
-              //           // _location,
-              //           //_speechToText.isNotListening ? _startListening : _stopListening,
-              //   tooltip: 'Listen',
+              // Container(
+              //   padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              //   color: Colors.deepPurple,
+              //   child: Row(
+              //     children: [
+              //       Expanded(
+              //         child: TextField(
+              //           controller: _controller,
+              //           style: TextStyle(color: Colors.white),
+              //         )
+              //       ),
+              //       IconButton(
+              //           onPressed: () {
+              //           _initSpeech();
+              //              sendMessage(_controller.text);
+              //              _controller.clear();
+              //           },
+              //           icon: Icon(Icons.send)
+              //       )
+              //     ],
+              //   ),
               // ),
-              
+              FloatingActionButton(
+                onPressed: _startListening,
+                        //_speechToText.isNotListening ? _startListening : _stopListening,
+                tooltip: 'Listen',
+                child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+              ),
 
               
             ],
@@ -140,50 +156,43 @@ class _MyAppState extends State<MyApp> {
   }
 
 
-  // void _sendEmail(){
-  //   var
-  //     Service_id='service_emp11gu',
-  //     Template_id='template_t12oyse',
-  //     User_id='Tz-1eK_bjtePLr2Tu';
-  //   var s = http.post(Uri.parse('https://api.emailjs.com/api/v1.0/email/send'),
-  //   headers: {
-  //       'origin':'http:localhost',
-  //       'Content-Type':'application/json'
-  //   },
-  //   body: jsonEncode({
-  //     'service_id': Service_id,
-  //     'user_id': User_id,
-  //     'template_id': Template_id,
-  //     'template_params':{
-  //       'name': 'Juan',
-  //       'name_contact': 'Heidy',
-  //       'message': 'SOS. Mi ubicacion actual:',
-  //       'sender_email': 'holmoscampos@gmail.com'
-  //     }
-  //   })
-  //   );
-  // }
+  sendMessage(String text) async { //toma el "text" y lo envia a dialogflow
+    if (text.isEmpty) {
+      print('Message esta vacio');
+    } else {
+      DetectIntentResponse response = await dialogFlowtter.detectIntent(
+          queryInput: QueryInput(text: TextInput(text: text, languageCode: 'es'))
+      );//aqui devuelve la respuesta de dialogflow hacia flutter
 
+      if (response.message == null) return;
 
-  
+      String? action = response.queryResult!.action; //el nombre de la accion
+      String ? msg = response.text; //response.message!.text!.text![0];
 
-
-
-  /// Esto tiene que suceder solo una vez por aplicación
-  void _initSpeech() async {
-    await _speechToText.initialize();
-  }
-
-
-  void _initLocation() async {
-    LocationPermission permission;
-    permission = await _geolocatorPlatform.checkPermission();
-    if(permission == LocationPermission.denied) {
-    permission = await _geolocatorPlatform.requestPermission();
+      _accionDialog(action!, msg!); //llama a la accion a realizar
     }
   }
 
-   /// Cada vez que inicie una sesión de reconocimiento de voz
+  _accionDialog(String action, String msg) async {
+    switch (action) {
+      case 'ubicacion': _speak(msg.replaceAll('[x]', 'Los Negros Santa Cruz'));
+        
+        break;
+      default: _speak('Podrias repetirlo por favor');
+    }
+  }
+
+  void _speak(String text) { //de texto a voz
+    flutterTts.speak(text);
+  }
+
+  /// Esto tiene que suceder solo una vez por aplicación
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    // setState(() {});
+  }
+
+  /// Cada vez que inicie una sesión de reconocimiento de voz
   void _startListening() async {
     await _speechToText.listen(onResult: _onSpeechResult);
     // setState(() {});
